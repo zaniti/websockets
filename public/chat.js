85e6b29e9c60eef3cb88e6590776f712c06b915a
Zniti-Codes <57224712+Zniti-Codes@users.noreply.github.com>
2020-05-07 03:57:54 +0000

    files

---
+++ public/chat.js
@@ -0,0 +1,33 @@
+//Make connection
+var socket = io.connect("http://localhost:4000");
+
+// Query DOM
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+var typing = document.getElementById('typing');
+
+//Emit events
+
+btn.addEventListener('click', function () {
+  socket.emit('chat',{
+    message: message.value,
+    handle: handle.value
+  });
+});
+
+message.addEventListener('keypress', function () {
+  socket.emit('typing', handle.value);
+});
+
+//Listen for events
+socket.on('chat', function(data){
+  message.value = '';
+  typing.innerHTML = '';
+  output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+  typing.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
